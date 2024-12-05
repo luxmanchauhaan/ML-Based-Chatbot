7bd75ffdcf9972c6a62c156837ac2f75cb8ae1fc
luxmanchauhaan <116090748+luxmanchauhaan@users.noreply.github.com>
2024-12-05 22:36:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, jsonify
+from nltk.stem import WordNetLemmatizer 
+from keras.models import load_model 
+import numpy as np 
+import pickle 
+import random
+import json 
+import nltk 
+
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [ lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]* len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    # print('Bag of Words:', bow)
+    res = model.predict(np.array([bow]))[0]
+    # print('Predicted Probabilities:', res)
+
+    ERROR_THRESHOLD = 0.01
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # print('Results:', results)
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    # print('Sorted Results:', results)
+
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    # print(intents_list[0])
+    for i in list_of_intents:
+        # print(tag)
+        if i['tag'] == tag:
+            result = random.choice (i['responses'])
+            break
+    return result
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    message = request.form["msg"]
+    # print(message)
+    ints = predict_class (message)
+    res = get_response (ints, intents)
+    return res
+
+if __name__ == '__main__':
+    app.run()
